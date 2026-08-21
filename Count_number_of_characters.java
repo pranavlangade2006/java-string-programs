6feee73452dd52f98499efc8802c88ba3cf191a6
pranav langade <pranavslangade@gmail.com>
2026-08-21 14:51:19 +0530

    Added string programs

---
+++ Count_number_of_characters.java
@@ -0,0 +1,10 @@
+class demo{
+	public static void main(String[]args){
+		String s = "java";
+		int count = 0;
+	for(int i=0;i<s.length();i++){
+		 count++; 	
+	}
+		System.out.println(count);
+}
+	}
