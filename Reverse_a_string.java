d73c34076321777c4414e6a36c10b1c733f47208
pranav langade <pranavslangade@gmail.com>
2026-08-22 00:52:08 +0530

    Added string programs

---
+++ Reverse_a_string.java
@@ -0,0 +1,12 @@
+class demo{
+	public static void main(String[]args){
+		String s= "abcd";
+		String rev = " ";
+	for(int i=s.length()-1 ; i>=0 ; i--){
+		rev = rev+ s.charAt(i);
+	}
+		System.out.println(rev);
+	}
+		}
+
+
