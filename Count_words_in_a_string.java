643031d82e5829911cc5c87af4cd857742fbf45f
pranav langade <pranavslangade@gmail.com>
2026-08-24 20:16:50 +0530

    Added string programs

---
+++ Count_words_in_a_string.java
@@ -0,0 +1,16 @@
+class demo{
+	public static void main(String[]args){
+		String s = "paranav santosh langade";
+		int count = 1;
+	for(int i=0 ; i<s.length() ; i++){
+		char ch = s.charAt(i);
+		
+	if(ch == ' '){
+		count++;
+	}
+		}
+	System.out.println(count);
+	}
+		}
+
+		
